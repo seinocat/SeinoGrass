0b4788a0270572785fd0f32beeb1eecd59b632c8
songqin <980047129@qq.com>
2024-07-22 18:36:10 +0800

    feat:算法修改

--- Unity/Assets/SeinoGrass/GrassBuilder.cs
+++ Unity/Assets/SeinoGrass/GrassBuilder.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using SeinoGrass.Utils;
 using Sirenix.OdinInspector;
 using Unity.Mathematics;
 using UnityEngine;
-using Random = Unity.Mathematics.Random;
 
 namespace SeinoGrass
 {
-    // [ExecuteInEditMode]
     public class GrassBuilder : MonoBehaviour
     {
         public GameObject Grass;
@@ -16,7 +13,7 @@ namespace SeinoGrass
         public float2 Corner0;
         public float2 Corner1;
 
-        [Button("生成1")]
+        [Button("生成")]
         public void Build()
         {
             Grass.SetActive(false);
@@ -31,28 +28,6 @@ namespace SeinoGrass
             }
         }
         
-        [Button("生成2")]
-        public void Build2()
-        {
-            Grass.SetActive(false);
-            Random random = new Random(123321);
-            List<float2> points = FastPoissonDiskSampler.SampleRectangle(
-                new float2(0, 0), new float2(10, 10), Radius, ref random);
-            GameObject root = new GameObject("GrassRoot");
-            for (int i = 0; i < points.Count; i++)
-            {
-                float2 point = points[i];
-                var go = Instantiate(Grass, new float3(point.x, 0, point.y), Quaternion.identity);
-                go.SetActive(true);
-                go.transform.SetParent(root.transform);
-            }
-        }
-
-        public void Update()
-        {
-            
-        }
-
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
--- Unity/Assets/SeinoGrass/Utils/PoissonDiskSampling.cs
+++ Unity/Assets/SeinoGrass/Utils/PoissonDiskSampling.cs
@@ -9,9 +9,17 @@ namespace SeinoGrass.Utils
         public const float TwoPi = 2 * math.PI;
         public static readonly float SqrtTwo = math.sqrt(2);
         
+        /// <summary>
+        /// 泊松圆盘采样，在指定区域均匀布点
+        /// </summary>
+        /// <param name="corner0">对角线起点</param>
+        /// <param name="corner1">对角线终点</param>
+        /// <param name="radius">最小半径</param>
+        /// <param name="k">迭代次数(默认30)</param>
+        /// <returns>点列表，每个点之间的距离大于radius</returns>
         public static List<float2> Sample(float2 corner0, float2 corner1, float radius, float k = 30)
         {
-            Random random = new Random(123321);
+            Random random = new Random(9527);
             List<float2> points = new List<float2>();
             List<float2> activePoints = new List<float2>();
             
@@ -22,7 +30,7 @@ namespace SeinoGrass.Utils
             
             float2?[,] grids = new float2?[width, height];
             float2 p0 = new float2(corner0.x + random.NextFloat(diagonal.x), corner0.y + random.NextFloat(diagonal.y));
-            AddPoint(grids, cellsize, corner0, p0);
+            AddPoint(ref grids, cellsize, corner0, p0);
             
             points.Add(p0);
             activePoints.Add(p0);
@@ -36,26 +44,24 @@ namespace SeinoGrass.Utils
                 for (int i = 0; i < k; i++)
                 {
                     float2 new_p = GetRandomPoint(p, radius, ref random);
-                    if (!IsValidPoint(grids, cellsize, width, height, radius, corner0, corner1, new_p))
+                    if (!IsValidPoint(ref grids, cellsize, width, height, radius, corner0, corner1, new_p))
                         continue;
                     
                     points.Add(new_p);
                     activePoints.Add(new_p);
-                    AddPoint(grids, cellsize, corner0, new_p);
+                    AddPoint(ref grids, cellsize, corner0, new_p);
                     found = true;
+                    break;
                 }
 
                 if (!found)
                     activePoints.RemoveAt(index);
-
-                if (points.Count > 1000)
-                    break;
             }
             
             return points;
         }
 
-        private static void AddPoint(float2?[,] grids, float cellsize, float2 p0, float2 p)
+        private static void AddPoint(ref float2?[,] grids, float cellsize, float2 p0, float2 p)
         {
             int x = (int)((p.x - p0.x) / cellsize);
             int y = (int)((p.y - p0.y) / cellsize);
@@ -65,27 +71,28 @@ namespace SeinoGrass.Utils
         private static float2 GetRandomPoint(float2 p, float radius, ref Random random)
         {
             float radians = TwoPi * random.NextFloat();
-            float new_radius = random.NextFloat(radius, 2 * radius);
+            float new_radius = radius * (1 + random.NextFloat());
             float x = p.x + new_radius * math.cos(radians);
             float y = p.y + new_radius * math.sin(radians);
             return new float2(x, y);
         }
 
-        private static bool IsValidPoint(float2?[,] grids, float cellsize, int width, int height, float radius, float2 p0, float2 p1, float2 p)
+        private static bool IsValidPoint(ref float2?[,] grids, float cellsize, int width, int height, float radius, float2 p0, float2 p1, float2 p)
         {
-            if (p.x < p0.x || p.x > p1.x || p.y < p0.y || p.y > p1.y)
+            if (p.x < p0.x || p.x >= p1.x || p.y < p0.y || p.y >= p1.y)
                 return false;
             
             int xindex = (int)((p.x - p0.x) / cellsize);
             int yindex = (int)((p.y - p0.y) / cellsize);
-            int i0 = math.max(0, xindex - 1);
-            int i1 = math.min(xindex + 1, width - 1);
-            int j0 = math.max(0, yindex - 1);
-            int j1 = math.min(yindex + 1, height - 1);
-                    
+            
+            int i0 = math.max(0, xindex - 2);
+            int i1 = math.min(xindex + 2, width);
+            int j0 = math.max(0, yindex - 2);
+            int j1 = math.min(yindex + 2, height);
+
             for (int i = i0; i < i1; i++)
                 for (int j = j0; j < j1; j++)
-                    if (grids[i, j].HasValue && math.length(grids[i, j].Value - p) < radius)
+                    if (grids[i, j].HasValue && math.distance(grids[i, j].Value, p) < radius)
                         return false;
 
             return true;
