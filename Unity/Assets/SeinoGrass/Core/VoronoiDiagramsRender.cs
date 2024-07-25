2a9288155e92989d33092ed69445455598f567e4
songqin <980047129@qq.com>
2024-07-25 14:30:42 +0800

    feat:voronoi shader

---
+++ Unity/Assets/SeinoGrass/Core/VoronoiDiagramsRender.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using Random = Unity.Mathematics.Random;
+
+namespace SeinoGrass.Core
+{
+    public class VoronoiDiagramsRender : MonoBehaviour
+    {
+        [PreviewField]
+        public Texture2D VoronoiDiagrams;
+        public int SeedCount;
+        public int2 Size;
+        public Material VoronoiMaterial;
+        
+        private List<Vector4> m_SeedArray;
+        private List<Vector4> m_ColorArray;
+        private NativeArray<Color32> m_VoronoiArray;
+
+        private static readonly int SeedCountProperty = Shader.PropertyToID("_SeedCount");
+        private static readonly int SeedArrayProperty = Shader.PropertyToID("_SeedArray");
+        private static readonly int ColorArrayProperty = Shader.PropertyToID("_ColorArray");
+        
+
+        private void Awake()
+        {
+            RandomSeed();
+            Generate();
+        }
+        
+        private void RandomSeed()
+        {
+            Random random = new Random(9800);
+            int count = SeedCount;
+            m_SeedArray = new List<Vector4>();
+            m_ColorArray = new List<Vector4>();
+            while (count-- > 0)
+            {
+                m_SeedArray.Add(new Vector4(random.NextFloat(), random.NextFloat()));
+                m_ColorArray.Add(new float4(random.NextInt(256), random.NextInt(256), random.NextInt(256), random.NextInt(256)));
+            }
+        }
+
+        [Button("重新生成")]
+        public void ReGenerate()
+        {
+            RandomSeed();
+            Generate();
+        }
+
+        private void Generate()
+        {
+            m_VoronoiArray = new NativeArray<Color32>(SeedCount, Allocator.Persistent);
+            
+            VoronoiMaterial.SetInt(SeedCountProperty, SeedCount);
+            VoronoiMaterial.SetVectorArray(SeedArrayProperty, m_SeedArray);
+            VoronoiMaterial.SetVectorArray(ColorArrayProperty, m_ColorArray);
+
+            RenderTexture rt = new RenderTexture(Size.x, Size.y, 0, GraphicsFormat.R8G8B8A8_UNorm);
+            rt.name = "Voronoi";
+            rt.Create();
+            
+            Graphics.Blit(null, rt, VoronoiMaterial);
+            Texture2D renderMap = new Texture2D(Size.x, Size.y, GraphicsFormat.R8G8B8A8_UNorm, TextureCreationFlags.None);
+            
+            RenderTexture.active = rt;
+            renderMap.ReadPixels(new Rect(0, 0, Size.x, Size.y), 0, 0);
+            renderMap.Apply();
+            RenderTexture.active = null;
+
+            VoronoiDiagrams = renderMap;
+            m_VoronoiArray = renderMap.GetPixelData<Color32>(0);
+        }
+    }
+}
---
+++ Unity/Assets/SeinoGrass/Resource/Shader/VoronoiDiagrams.shader
@@ -0,0 +1,75 @@
+﻿Shader "Algorithm/VoronoiDiagrams"
+{
+	Properties
+	{
+
+	}
+	
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			HLSLPROGRAM
+
+			#pragma enable_d3d11_debug_symbols
+            #pragma multi_compile_instancing
+
+            #include  "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+			#pragma vertex vert
+            #pragma fragment frag
+
+			CBUFFER_START(UnityPerMaterial)
+			int _SeedCount;
+			int _Width;
+			int _Height;
+			uniform float4 _SeedArray[512];
+			uniform float4 _ColorArray[512];
+			CBUFFER_END
+
+			struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+			
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = TransformObjectToHClip(v.vertex.xyz);
+	            o.uv = v.uv;
+	            return o;
+			}
+
+			float4 frag(v2f f) : SV_Target
+			{
+				float4 color = _ColorArray[0];
+				float minLen = 1;
+				for (int i = 0; i < _SeedCount; i++)
+				{
+					float len = length(f.uv - _SeedArray[i].xy);
+					if (len < minLen)
+					{
+						minLen = len;
+						color = _ColorArray[i];
+					}
+				}
+				
+				return color/255;
+			}
+
+			ENDHLSL
+		}
+	}
+}
